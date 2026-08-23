64f9946fd2815509bb1c70a794fc8f4b7e7c3430
Elakiya <elakiyakamal7@gmail.com>
2026-08-23 18:13:39 +0530

    Update state module

--- state.js
+++ state.js
@@ -14,20 +14,4 @@ export function getCurrentUser() {
 export function clearCurrentUser() {
     currentUser = null;
     localStorage.removeItem("loggedInUser");
-}// Application State
-
-let currentUser = JSON.parse(localStorage.getItem("loggedInUser")) || null;
-
-export function setCurrentUser(user) {
-    currentUser = user;
-    localStorage.setItem("loggedInUser", JSON.stringify(user));
-}
-
-export function getCurrentUser() {
-    return currentUser;
-}
-
-export function clearCurrentUser() {
-    currentUser = null;
-    localStorage.removeItem("loggedInUser");
 }
